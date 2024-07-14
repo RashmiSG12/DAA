b679773f14f319002a0abb0e1a4b76ebb0b625d3
Rashmi S G <157728640+RashmiSG12@users.noreply.github.com>
2024-07-14 13:10:02 +0530

    Update quick-sort.py

--- quick-sort.py
+++ quick-sort.py
@@ -13,10 +13,14 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def quickSort(arr):
-    # Write your code here
 
-if _name_ == '_main_':
+def quickSort(arr):
+    left = [i for i in arr if i < arr[0]]
+    right = [i for i in arr if i > arr[0]]
+    equal = [i for i in arr if i == arr[0]]
+    return left + equal + right
+
+if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input().strip())
