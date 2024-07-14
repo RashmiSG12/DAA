ab5963de80c3af304128c75ca282f55d270b02ce
Rashmi S G <157728640+RashmiSG12@users.noreply.github.com>
2024-07-14 13:58:48 +0530

    Update quick-sort.py

--- quick-sort.py
+++ quick-sort.py
@@ -1,6 +1,3 @@
-#!/bin/python3
-
-import math
 import os
 import random
 import re
@@ -12,13 +9,31 @@ import sys
 # The function is expected to return an INTEGER_ARRAY.
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
-
-
 def quickSort(arr):
-    left = [i for i in arr if i < arr[0]]
-    right = [i for i in arr if i > arr[0]]
-    equal = [i for i in arr if i == arr[0]]
-    return left + equal + right
+     quickSortH(arr,0,len(arr)-1)
+     return arr
+
+def quickSortH(arr,i,j):
+    if i < j:
+        loc = partition(arr, i, j)
+        quickSortH(arr, i, loc-1)
+        quickSortH(arr, loc+1, j)
+
+def partition(arr, i, j):
+    pivot = arr[i]
+    low = i
+    high = j
+    while low <= high:
+        while low <= high and arr[low] <= pivot:
+            low += 1
+        while high >= low and arr[high] > pivot:
+            high -= 1
+        if low < high:
+            arr[low], arr[high] = arr[high], arr[low]
+    arr[i], arr[high] = arr[high], arr[i]
+    return high
+
+   
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -33,3 +48,5 @@ if __name__ == '__main__':
     fptr.write('\n')
 
     fptr.close()
+
+
